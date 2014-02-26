089928d40c44c37f9313784a083c2bfd9389ccfa
James Mortemore <james.mortemore@clock.co.uk>
2014-02-26 14:25:24 +0000

    Added employee images

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 var express = require('express')
   , http = require('http')
   , path = require('path')
+  , _ = require('lodash')
   , getSpreadsheet = require('./lib/get-spreadsheet')
   , processSpreadsheet = require('./lib/process-spreadsheet')
   , getHolidays = require('./lib/get-holidays')
@@ -28,14 +29,42 @@ if ('development' === app.get('env')) {
   app.use(express.errorHandler())
 }
 
+var absenceOptions =
+    { debug: true
+    , oauth:
+      { email: process.env.EMAIL_ADDRESS
+      , keyFile: process.env.PRIVATE_KEY
+      }
+    , spreadsheetId: process.env.SPREADSHEET_ID
+    , worksheetId: process.env.WORKSHEET_ID
+    }
+  , employeeImageOptions =
+    { debug: true
+    , oauth:
+      { email: process.env.EMAIL_ADDRESS
+      , keyFile: process.env.PRIVATE_KEY
+      }
+    , spreadsheetId: process.env.SPREADSHEET_ID
+    , worksheetId: process.env.EMPLOYEE_WORKSHEET
+    }
+  , employeeImages = {}
+
 // Uncomment for testing purposes so that the spreadsheet gets loaded
 // when the app runs
 // getSpreadsheet(function (err, spreadsheet) {
 //   console.log(processSpreadsheet(spreadsheet));
 // })
 
+getSpreadsheet(employeeImageOptions, function (error, spreadsheet) {
+  if (error) return console.error(error)
+
+  _.each(spreadsheet, function (data) {
+    employeeImages[data['1']] = data['2']
+  })
+})
+
 app.get('/', function (req, res) {
-  getSpreadsheet(function (error, spreadsheet) {
+  getSpreadsheet(absenceOptions, function (error, spreadsheet) {
     if (error) {
       console.error(error)
       return res.send(500, { error: error })
@@ -47,7 +76,8 @@ app.get('/', function (req, res) {
         return res.send(500, { error: error })
       }
 
-      res.render('index', { absences: processSpreadsheet(spreadsheet), holidays: holidays })
+      res.render('index', { absences: processSpreadsheet(spreadsheet)
+        , holidays: holidays, employeeImages: employeeImages })
     })
 
   })
