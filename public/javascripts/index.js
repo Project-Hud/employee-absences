4bb99f776f4f39b6d149742084917cbf4dcc4196
Tim Gleeson <tim.j.gleeson@gmail.com>
2014-03-10 17:14:59 +1100

    add auto rotating to show more data easily

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,1 +1,25 @@
-(function () {})()
+(function () {
+
+  function hide($el) {
+    $el.fadeOut()
+  }
+
+  function show($el) {
+    $el.fadeIn()
+  }
+
+  function next() {
+    var $currentPanel = $('.js-panel:visible')
+      , $upcomingPanel = $currentPanel.next('.js-panel')
+
+    if ($upcomingPanel.length === 0) {
+      $upcomingPanel = $('.js-panel:first-child')
+    }
+
+    hide($currentPanel)
+    show($upcomingPanel)
+  }
+
+  setInterval(next, 5000)
+
+})()
