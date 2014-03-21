47d987e7a1af720c717df5026e7e435b576afec8
Mark Bensley <mark.bensley@clock.co.uk>
2014-03-21 12:29:33 +0000

    Initial commit absence

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,25 +1,96 @@
 (function () {
 
-  function hide($el) {
-    $el.fadeOut()
+
+
+/*var position = $('.sliderOne__title').offset();
+var positionLeft = position.left*/
+
+/*if (positionLeft > 140){
+  slideRight()
+}*/
+/*
+setInterval(function(){
+  slideLeft()
+}, 5000)
+
+setInterval(function(){
+  slideRight()
+}, 10000)*/
+
+
+numberOfSlides = $('.slider--slide').length
+
+
+sliderWidth = 300
+currentSlide = 0
+
+containerWidth = sliderWidth * numberOfSlides
+
+numberOfDays = 1 + Math.floor(Math.random() * 6)
+
+$('.slider-container').css("width", containerWidth)
+
+
+
+function slideTo(num){
+  $('.slider-container').animate({left: -(sliderWidth * num / 2)})
+  currentSlide = num
+}
+
+for (var i = 0; i < numberOfSlides; i++) {
+  $('.controls').append('<li></li>')
+
+}
+
+$('.days-left').text(numberOfDays)
+
+function slideNext(){
+  if(currentSlide === (numberOfSlides - 1)) {
+    slideTo(0)
+  } else {
+    slideTo(currentSlide + 1)
+
   }
+}
 
-  function show($el) {
-    $el.fadeIn()
-  }
 
-  function next() {
-    var $currentPanel = $('.js-panel:visible')
-      , $upcomingPanel = $currentPanel.next('.js-panel')
+function slidePrev(){
+  slideTo(currentSlide - 1)
+}
 
-    if ($upcomingPanel.length === 0) {
-      $upcomingPanel = $('.js-panel:first-child')
-    }
+function changeOpacity(){
+  $('.title__holiday').animate({opacity: 1})
+}
 
-    hide($currentPanel)
-    show($upcomingPanel)
-  }
+setInterval(function (){
+  slideNext()
+}, 5000)
 
-  setInterval(next, 5000)
+
+
+
+
+
+  // function hide($el) {
+  //   $el.fadeOut()
+  // }
+
+  // function show($el) {
+  //   $el.fadeIn()
+  // }
+
+  // function next() {
+  //   var $currentPanel = $('.js-panel:visible')
+  //     , $upcomingPanel = $currentPanel.next('.js-panel')
+
+  //   if ($upcomingPanel.length === 0) {
+  //     $upcomingPanel = $('.js-panel:first-child')
+  //   }
+
+  //   hide($currentPanel)
+  //   show($upcomingPanel)
+  // }
+
+  // setInterval(next, 5000)
 
 })()
