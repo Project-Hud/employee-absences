882964e29774dc37fdaf7c68a5d5664825351ae1
Matthew Harrison-Jones <matthojo@gmail.com>
2014-03-27 14:01:33 +0000

    Make responsive and small design improvements

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,48 +1,55 @@
 (function () {
 
-var numberOfSlides = $('.slider--slide').length
-  , sliderWidth = 370
-  , currentSlide = 0
-  , containerWidth = sliderWidth * numberOfSlides
-  , sliderContainer = $('.slider-container')
+  var numberOfSlides = $('.slider--slide').length
+    , sliderContainer = $('.slider-container')
+    , sliderWidth = $(window).width()
+    , currentSlide = 0
+    , containerWidth = sliderWidth * numberOfSlides
 
-function init() {
-  sliderContainer.css("width", containerWidth)
 
-  for (var i = 0; i < numberOfSlides; i++) {
-    $('.controls').append('<li class=\"slide-control\"></li>')
+  function init() {
+    sliderContainer.css("width", containerWidth)
 
-  }
+    for (var i = 0; i < numberOfSlides; i++) {
+      $('.controls').append('<li class=\"slide-control\"></li>')
 
-  slideTo(0)
+    }
 
-  setInterval(function (){
-    slideNext()
-  }, 5000)
-}
-
-function slideTo(num){
-  sliderContainer.animate({left: -(sliderWidth * num )}, 700)
-  $('.slider--slide.is-active').removeClass('is-active')
-  $('.slider--slide:eq(' + num + ')').addClass('is-active')
-  $('.slide-control.slide-is-active').removeClass('slide-is-active')
-  $('.slide-control:eq(' + num + ')').addClass('slide-is-active')
-  currentSlide = num
-}
-
-function slideNext(){
-  if(currentSlide === (numberOfSlides - 1)) {
     slideTo(0)
-  } else {
-    slideTo(currentSlide + 1)
 
+    setInterval(function (){
+      slideNext()
+    }, 5000)
   }
-}
 
-function slidePrev(){
-  slideTo(currentSlide - 1)
-}
+  function slideTo(num){
+    sliderContainer.animate({left: -(sliderWidth * num )}, 700)
+    $('.slider--slide.is-active').removeClass('is-active')
+    $('.slider--slide:eq(' + num + ')').addClass('is-active')
+    $('.slide-control.slide-is-active').removeClass('slide-is-active')
+    $('.slide-control:eq(' + num + ')').addClass('slide-is-active')
+    currentSlide = num
+  }
 
-init()
+  function slideNext(){
+    if(currentSlide === (numberOfSlides - 1)) {
+      slideTo(0)
+    } else {
+      slideTo(currentSlide + 1)
+
+    }
+  }
+
+  function slidePrev(){
+    slideTo(currentSlide - 1)
+  }
+
+  if ($('.stacked').length > 0) {
+    console.log('stacked')
+    sliderContainer.css('width', $('.stacked').width())
+  } else {
+    console.log('not-stacked', $('.stacked'))
+    init()
+  }
 
 })()
